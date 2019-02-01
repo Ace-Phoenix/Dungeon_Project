2b1f36ef3b771eb7f1b1fd96a276a05d1761d773
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-02-01 10:52:25 -0500

    master

---
+++ Dungeon.js/Stage.js
@@ -0,0 +1,5 @@
+// this is going to be the stages.
+// u will start in a room and u have to kill all enemys to go through
+// the door and the dungeon function will have to be passed to things
+// because it will need to know how many enemys remaijn and how far the player
+// can go so the player cant go outside the map
---
+++ Master.js
@@ -0,0 +1,2 @@
+// this is going to be the functions and variables that will be used
+// everything if it can be (anyhintg that reapets and can be put into a function)
