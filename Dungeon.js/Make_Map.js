e464a9821aff740e4f85ffc96220b0f19cb03a9c
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-02-04 09:49:16 -0500

    make map update

--- Dungeon.js/Make_Map.js
+++ Dungeon.js/Make_Map.js
@@ -1,2 +1,21 @@
 // this is going to be the dungeon and is going to be passed to the person and
 // enemies
+// this si going to be the grid to pit the dungeon on
+
+function grid(number, element, name){
+var number = number
+var make = [];
+for (var i = 0; i < number; i++) {
+  make[i] = []
+
+}
+for (var j = 0; j < number; j++) {
+  make[i][j] = new element(name, [i,j])
+}
+return make;
+}
+var map = grid(50, Dungeon)
+function draw(grid){
+  var borders = "";
+  var
+}
--- Master.js
+++ Master.js
@@ -1,2 +1,9 @@
 // this is going to be the functions and variables that will be used
 // everything if it can be (anyhintg that reapets and can be put into a function)
+
+class Dungeon {
+  constructor() {
+    
+
+  }
+}
