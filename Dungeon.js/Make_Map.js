f901a2c717c6987ce4ec228483744ade00640985
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-02-04 10:15:16 -0500

    maps change with draw

--- Dungeon.js/Make_Map.js
+++ Dungeon.js/Make_Map.js
@@ -2,29 +2,7 @@
 // enemies
 // this si going to be the grid to pit the dungeon on
 
-<<<<<<< HEAD
 function grid(){
-=======
-function grid(number, element, name){
-<<<<<<< HEAD
-var number = number
-var make = [];
-for (var i = 0; i < number; i++) {
-  make[i] = []
-
-}
-for (var j = 0; j < number; j++) {
-  make[i][j] = new element(name, [i,j])
-}
-return make;
-}
-var map = grid(50, Dungeon)
-function draw(grid){
-  var borders = "";
-  var
-}
-=======
->>>>>>> f37f72891b2b16c9af4df2524bb405114e4775ff
 var make = [];
 for (var i = 0; i < make.length; i++) {
   make[i]
@@ -39,12 +17,29 @@ return make[i][j];
 fucntion draw(grid){
   var text = " ";
   var lengt = grid.length;
-  var top =  
-  for (var i = 0; i < array.length; i++) {
-    array[i]
+  var top = lengt*2;
+  text += "+";
+  // this makes the top border
+  for (var i = 0; i < lengt; i++) {
+    top = text += "--"
   }
-  for (var i = 0; i < array.length; i++) {
-    array[i]
+  text += "+<BR>";
+
+  for (var i = 0; i < lengt; i++) {
+    text += "| "
+
+  for (var j = 0; j < array.length; j++) {
+    grid[i][j];
+    text += " " + grid[i][j];
   }
+text += "<BR>";
 }
->>>>>>> a00a89e1018b1119ccf60be493d3594b080b1b44
+text += "+";
+
+for (var i = 0; i < lengt; i++) {
+  top = text += "--";
+}
+text += "+"
+return text;
+
+}
