325811ed7e48d7c983b1b781f0d58ab9e1942b62
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-02-04 10:53:00 -0500

    this si commenting on map

--- Dungeon.js/Make_Map.js
+++ Dungeon.js/Make_Map.js
@@ -1,30 +1,40 @@
 // this is going to be the dungeon and is going to be passed to the person and
 // enemies
-// this si going to be the grid to pit the dungeon on
+// this si going to be the grid to put the dungeon on
 
 function grid(){
+  // this is going to be the empty array that we can pcuh and pull from
 var make = [];
 for (var i = 0; i < make.length; i++) {
   make[i]
 
-}
+
 for (var j = 0; j < make.length; j++) {
   make[j]
 }
+}
+// this is going to be the grid[i][j
 return make[i][j];
 }
-
+// this is going to be the fucntion thats draws the grid
 function draw(grid){
+  // this is going to be the text variable
   var text = " ";
+// this is the variable for the grid length so we can store it
   var lengt = grid.length;
+  // this is going to be the legth of the gird times 2
   var top = lengt*2;
+  // this is so text knows what to use for the grid
   text += "+";
   // this makes the top border
   for (var i = 0; i < lengt; i++) {
     top = text += "--"
   }
+  // this is so the grid is square for now
+  // thats why theres a brake <BR>
   text += "+<BR>";
 
+// this for satement is for the sides of the grid usaing pipes
   for (var i = 0; i < lengt; i++) {
     text += "| "
 
@@ -40,6 +50,7 @@ for (var i = 0; i < lengt; i++) {
   top = text += "--";
 }
 text += "+"
+// return the text strings so its appears on the screen
 return text;
 
 }
