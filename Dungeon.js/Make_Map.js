5ccc3f908139939535aea9d1de0423eb29ab25b2
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-02-04 09:33:14 -0500

    make map update

--- Dungeon.js/Make_Map.js
+++ Dungeon.js/Make_Map.js
@@ -1,2 +1,15 @@
 // this is going to be the dungeon and is going to be passed to the person and
 // enemies
+// this si going to be the grid to pit the dungeon on
+
+function grid(){
+var make = [];
+for (var i = 0; i < make.length; i++) {
+  make[i]
+
+}
+for (var j = 0; j < make.length; j++) {
+  make[j]
+}
+return make[i][j];
+}
