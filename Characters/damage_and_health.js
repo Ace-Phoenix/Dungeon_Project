563963fdb385842285009d578502a74752ca3553
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-02-05 10:43:26 -0500

    enemy player damage and health and master code base/start

--- Characters/damage_and_health.js
+++ Characters/damage_and_health.js
@@ -1,18 +1,47 @@
 // this is going to be to damage and health weapons and items for eneimes and
 // for the heros
 // this will need the person class and atributes for cetain things
+//P = Player on the grid
+//E = Enemy on the grid
 class Health_And_Damage extends Dungeon{
 constructor(){
 
 }
-}
 //this will be what gets increased
 //over how may kills the player gets
-function healthPlus(){
+healthPlus_P(){
+if(enemy_killed == true){
+  new_P_health += 75
 
+}
+var enemy_killed = false
 }
 //damage will increase with each kill
 //and stage / room compleate
-function damagePlus(){
+damagePlus_P(){
+  if(dungeon_room_cleared == true){
+    new_P_damage += 5
 
+  }
+  var dungeon_room_cleared = false
+}
+//this will be what gets increased
+//over how may rooms the player completes
+healthPlus_E(){
+  if(enemy_killed == true){
+    new_P_health += 100
+
+  }
+  var enemy_killed = false
+}
+//damage will increase with each
+// stage / room compleate
+damagePlus_E(){
+  if(dungeon_room_cleared == true){
+    new_E_damage += 5
+
+  }
+  var dungeon_room_cleared = false
+
+}
 }
