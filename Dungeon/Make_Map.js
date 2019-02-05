07218935b6c672913854f3ee56e12bf54c7cd927
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-02-05 10:46:29 -0500

    got most of the grid and draw function done

--- Dungeon/Make_Map.js
+++ Dungeon/Make_Map.js
@@ -2,33 +2,32 @@
 // enemies
 // this si going to be the grid to put the dungeon on
 
-function grid(){
+function grid(number, element, name = "o"){
   // this is going to be the empty array that we can pcuh and pull from
-var make = [];
-for (var i = 0; i < make.length; i++) {
-  make[i]
-
-
-for (var j = 0; j < make.length; j++) {
-  make[j]
-}
-}
+    var number = number;
+    var make = [];
+  for (var i = 0; i < make.length; i++) {
+    make.push([]);
+  for (var j = 0; j < make.length; j++) {
+    make[i].push(new element(name,[i,j]));
+    }
+  }
 // this is going to be the grid[i][j
-return make[i][j];
-}
+    return make;
+  }
 // this is going to be the fucntion thats draws the grid
 function draw(grid){
   // this is going to be the text variable
-  var text = " ";
+  var text = "";
 // this is the variable for the grid length so we can store it
   var lengt = grid.length;
   // this is going to be the legth of the gird times 2
-  var top = lengt*2;
+  var top = lengt *2;
   // this is so text knows what to use for the grid
   text += "+";
   // this makes the top border
   for (var i = 0; i < lengt; i++) {
-    top = text += "--"
+    top = text += "--";
   }
   // this is so the grid is square for now
   // thats why theres a brake <BR>
@@ -36,25 +35,25 @@ function draw(grid){
 
 // this for satement is for the sides of the grid usaing pipes
   for (var i = 0; i < lengt; i++) {
-    text += "| "
+    text += "| ";
 
   for (var j = 0; j < array.length; j++) {
     grid[i][j];
     text += " " + grid[i][j];
   }
-text += "<BR>";
+text += "|<BR>";
 }
 text += "+";
 
 for (var i = 0; i < lengt; i++) {
   top = text += "--";
 }
-text += "+"
+text += "+";
 // return the text strings so its appears on the screen
 return text;
 
 }
-var grid = grid(30, Person);
+var grid = grid(30);
 // this is what physiaclly draws the grid
 document.getElementById("blah").innerHTML = draw(grid);
 // this is is calling the grid to the html
