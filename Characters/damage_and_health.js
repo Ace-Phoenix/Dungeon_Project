3e1f05a52b4d12af841074eb6f2fcb5e44258aee
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-02-05 09:54:28 -0500

    comments and functions

--- Characters/damage_and_health.js
+++ Characters/damage_and_health.js
@@ -1,11 +1,10 @@
-// this is going to be te damage and health weapons and items for eneimes and
+// this is going to be to damage and health weapons and items for eneimes and
 // for the heros
 // this will need the person class and atributes for cetain things
 class Health_And_Damage extends Dungeon{
-construuctor(){
+constructor(){
 
 }
-
 }
 //this will be what gets increased
 //over how may kills the player gets
@@ -15,9 +14,5 @@ function healthPlus(){
 //damage will increase with each kill
 //and stage / room compleate
 function damagePlus(){
-
-}
-//kill counter for damagePlus and healthPlus to work proberly
-function kills(){
 
 }
