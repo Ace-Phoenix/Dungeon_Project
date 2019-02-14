76b7f4be484f8670ac71a4b526cd74c206fbe038
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-02-14 18:36:27 -0500

    don know what you did but now its broken even more i have no idea i tried some things many things non that neem to get rid of the commas but i know what forms them just dont know how to get rid of them there formed in var grid = grid({x:#,y:#}, Dungeon} so well have to ask about that

--- Dungeon/Make_Map.js
+++ Dungeon/Make_Map.js
@@ -2,7 +2,7 @@
 // enemies
 // this si going to be the grid to put the dungeon on
 
-function grid(size, element, name = "x"){
+function grid(size, element, name = "&#9926"){
   // this is going to be the empty array that we can pcuh and pull from
     //var number = number;
     // this is the empty array that u push and pull from
@@ -14,8 +14,7 @@ function grid(size, element, name = "x"){
   for (var j = 0; j < size.y; j++) {
     // this si to push the new element into the array so it has the name and
     // grid locations
-    make[i].push(new element(name,[i,j]));
-
+    make.push(new element(name,[i,j]));
     }
 
   }
@@ -33,11 +32,14 @@ function draw(grid){
   // this is going to be the legth of the gird times 2
   var top = lengt *2;
 
-  var fixtop = grid[0].length;
+  var fixtop = grid.length;
+  console.log(fixtop + " plz1");
   // this is so text knows what to use for the grid
   text += design;
   // this makes the top border
   for (var i = 0; i < fixtop; i++) {
+    console.log(fixtop + " plz1");
+
     top = text += design;
   }
   // this is so the grid is square for now
@@ -48,18 +50,24 @@ function draw(grid){
   for (var i = 0; i < lengt; i++) {
     text += background;
 
-  for (var j = 0; j < grid[0].length; j++) {
-    text += "" + grid[i][j];
+  for (var j = 0; j < grid.length; j++) {
+    text += "" + grid[i,j];
 
   }
 text += background + "<BR>";
 }
-text += design;
+text += design + "<BR>";
 
 for (var i = 0; i < fixtop; i++) {
-  top = text += design;
+  top = text += design + "<BR>";
+  console.log(fixtop + " plz1");
+
 }
-text += design;
+text += design + "<BR>";
+console.log(background + "bg");
+console.log(design + "design");
+console.log(text + "text");
+console.log(text.length + "     _text");
 
 
 
@@ -68,7 +76,6 @@ return text;
 
 }
 
-<<<<<<< HEAD
 class Space{
   constructor(x, y, w, h, grid){
     this.x = x;
@@ -101,56 +108,21 @@ class Room extends Space {
     }
   }
 
-  function makeroom(grid, name = ""){
-    var txt = ""
-    var square = [];
-    var room = new Room;
-    room.x = Math.random(room.x) * grid.length;
-    room.y = Math.random(room.y) * grid.length;
-    room.h = Math.random(room.h) * 10;
-    room.w = Math.random(room.w) * 10;
-    for (var i = 0; i < room.h; i++) {
-      square[i] = []
-      for (var j = 0; j < room.w; j++) {
-        square[i][j] = grid[i][j];
-=======
-// class Space{
-//   constructor(x, y, w, h){
-//     this.x = x;
-//     this.y = y;
-//     this.w = w;
-//     this.h = h;
-//     this.center = {
-//       x:Math.floor(x + (w / 2)),
-//       y:Math.floor(y + (h / 2)),
-//     }
-//   }
-//   // drawpath(c, space, grid){
-//   //   //const pattern = c.create pattern('reapeat')
-//   //   c.beginpath()
-//   //   c.lineWidth = TILE;
-//   //   c.strokeStyle = pattern;
-//   //   c.moveTo(this.center.x *TILE, this.center.y *TILE)
-//   //   c.lineTo(space.center.x *TILE, space.center.y *TILE)
-//   //   c.stroke()
-//   //   }
-//   // }
-// }
-//
-// class Room extends Space {
-//   consturctor(space, name = " "){
-//     //super();
-//     this.x = space.x + random(1, Math.floor(space.w/3))
-//     this.y = space.y + random(1, Math.floor(space.h/3))
-//     this.w = space.w - (this.x - space.x)
-//     this.h = space.h - (this.y - space.y)
-//     this.w -= random(0, this.w/4)
-//     this.h -= random(0, this.h/4)
-//     }
-//   }
+  // function makeroom(grid, name = ""){
+  //   var txt = ""
+  //   var square = [];
+  //   var room = new Room;
+  //   room.x = Math.random(room.x) * grid.length;
+  //   room.y = Math.random(room.y) * grid.length;
+  //   room.h = Math.random(room.h) * 10;
+  //   room.w = Math.random(room.w) * 10;
+  //   for (var i = 0; i < room.h; i++) {
+  //     square[i] = []
+  //     for (var j = 0; j < room.w; j++) {
+  //       square[i][j] = grid[i][j];
 
   function makeroom(grid, name = " "){
-    var txt = "l";
+    var txt = "";
     var square = [];
     var room = new Room;
     room.x = Math.random(room.x) * grid.length/2;
@@ -158,38 +130,40 @@ class Room extends Space {
     room.h = Math.random(room.h) * grid.length/2;
     room.w = Math.random(room.w) * grid.length/2;
     for (var i = 0; i < room.h; i++) {
-      console.log(txt)
+      console.log(txt + " 1")
+      grid[i] = []
+      console.log(grid[i] + " _gri d?");
+
       txt += "";
+      grid[i].push(txt)
+      console.log(grid[i] + " _ grid?");
+
       for (var j = 0; j < room.w; j++) {
-        console.log(txt)
-        txt += "" + grid[i][j];
->>>>>>> 4cd5498cd272e3fa2cc61b66e35e06f4a7e705fa
+        console.log(txt + " 2")
+        txt += "" + grid[i];
+        console.log(grid[i] + " _g rid?");
+
+        console.log(txt + " 3")
+
       }
     }
-    grid.splice(grid[i,j])
-square.push(grid[0])
     console.log(j);
     console.log(i);
     console.log(room)
-<<<<<<< HEAD
-    console.log(square + "i");
-    return square;
-  }
-var grid = grid({x:35,y:40}, Dungeon);
-=======
-    return grid;
-  }
+    console.log(square + "square");
+    console.log(txt + " txt");
+var grid =  String(grid({x:35,y:40}, Dungeon))
+console.log(grid + " _grid?");
 
-var grid = grid({x:60,y:100}, Dungeon);
-
->>>>>>> 4cd5498cd272e3fa2cc61b66e35e06f4a7e705fa
+    return grid
+  }
 // this is what physiaclly draws the grid
 document.getElementById("blah").innerHTML = draw(grid);
 // this is is calling the grid to the html
 // this is my automatic update variable
     // this calls the update function
 document.getElementById("button").onclick = function(){
-  document.getElementById("blah").innerHTML = makeroom(grid);
+  document.getElementById("blah").innerHTML = makeroom(grid, name = " ");
 console.log("Clicky");
   // this calls the update function
 }
--- Dungeon/Stage.js
+++ Dungeon/Stage.js
@@ -3,8 +3,3 @@
 // the door and the dungeon function will have to be passed to things
 // because it will need to know how many enemys remaijn and how far the player
 // can go so the player cant go outside the map
-class Rooom extends Dungeon{
-  constructor(){
-
-  }
-}
