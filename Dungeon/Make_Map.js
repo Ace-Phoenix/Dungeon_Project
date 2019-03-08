9fced909f685bd7b574c975431a2c9d96f061cef
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-03-08 10:40:29 -0500

    were almost done now

--- Dungeon/Make_Map.js
+++ Dungeon/Make_Map.js
@@ -1,40 +1,39 @@
-// this is going to be the dungeon and is going to be passed to the person and
-// enemies
-// this si going to be the grid to put the dungeon on
+  // this is going to be the dungeon and is going to be passed to the person and
+  // enemies
+  // this si going to be the grid to put the dungeon on
 
-function grid(size, element, name = "x"){
-  // this is going to be the empty array that we can pcuh and pull from
+  function grid(size, element, name = "x"){
+    // this is going to be the empty array that we can pcuh and pull from
     //var number = number;
     // this is the empty array that u push and pull from
-    var make = [];
+  var make = [];
     // this is to push into the make array
   for (var i = 0; i < size.x; i++) {
     // this is array.push
     make.push([]);
   for (var j = 0; j < size.y; j++) {
-    // this si to push the new element into the array so it has the name and
+    // this is to push the new element into the array so it has the name and
     // grid locations
     make[i].push(new element(name,[i],[j]));
-
     }
   }
-// this is going to be the grid[i][j
+    // this is going to be the grid[i][j
     return make;
   }
 
-// this is going to be the fucntion thats draws the grid
-function draw(grid){
-  var design = "&#8943";
-  var background = "&#8942";
+    // this is going to be the fucntion thats draws the grid
+    function draw(grid){
+    var design = "&#8943";
+    var background = "&#8942";
   // this is going to be the text variable
-  var text = "";
-// thSis the variable for the grid length so we can store it
-  var lengt = grid.length;
+    var text = "";
+  // this the variable for the grid length so we can store it
+    var lengt = grid.length;
   // this is going to be the legth of the gird times 2
-  var top = lengt *2;
-  var fixtop = grid[0].length;
+    var top = lengt + 20;
+    var fixtop = grid[0].length;
   // this is so text knows what to use for the grid
-  text += design;
+    text += design;
   // this makes the top border
   for (var i = 0; i < fixtop; i++) {
     top = text += design;
@@ -44,10 +43,9 @@ function draw(grid){
   // thats why theres a brake <BR>
   text += design + "<BR>";
 
-// this for satement is for the sides of the grid usaing pipes
+  // this for satement is for the sides of the grid usaing pipes
   for (var i = 0; i < lengt; i++) {
     text += background;
-
 
   for (var j = 0; j < grid[0].length; j++) {
     text += "" + grid[i][j] ;
@@ -63,85 +61,14 @@ function draw(grid){
 
   }
   text += design;
-// return the text strings so its appears on the screen
+  // return the text strings so its appears on the screen
   return text;
   }
-// function roomx(grid, draw){
-//   var room = {
-//     x : Math.random() *grid.length,
-//   }
-//   var roomxfloor = Math.floor(room.x);
-//   if(grid.length >= roomxfloor && grid.length <= roomxfloor *2){
-//     grid[roomxfloor]= " ";
-//     console.log(grid[roomxfloor] + " this is grid roomx floor")
-//   }
-//   return grid[roomxfloor];
-// }
-//
-// function roomy(grid, draw){
-//   var room = {
-//     y : Math.random() *grid.length,
-//   }
-//   var roomyfloor = Math.floor(room.y);
-//   if(grid.length >= roomyfloor && grid.length <= roomyfloor *2){
-//     grid[roomyfloor] === " ";
-//     console.log(grid[roomyfloor] + " this is grid roomy floor")
-//   }
-//   return grid[roomyfloor];
-// }
-// function drawroom(roomx, roomy, grid){
-//   var room = {
-//     x : Math.random() *grid.length,
-//     y : Math.random() *grid.length,
-//   }
-//   var roomxfloor = Math.floor(room.x);
-//   var roomyfloor = Math.floor(room.y);
-//   roomx(grid, draw);
-//   roomy(grid, draw);
-//   return draw(grid);
-// }
-//
-//
-//  class Space{
-//    constructor(x, y, w, h){
-//      this.x = x;
-//      this.y = y;
-//      this.w = w;
-//      this.h = h;
-//      this.center = {
-//        x:Math.floor(x + (w / 2)),
-//        y:Math.floor(y + (h / 2)),
-//      }
-//    }
-//   drawpath(c, space, grid){
-//  const pattern = c.create pattern('reapeat')
-//  c.beginpath()
-//  c.lineWidth = TILE;
-//  c.strokeStyle = pattern;
-//  c.moveTo(this.center.x *TILE, this.center.y *TILE)
-//  c.lineTo(space.center.x *TILE, space.center.y *TILE)
-//  c.stroke()
-//   }
-//  }
-//  }
-//
-// class Room extends Space {
-//   consturctor(space, name = " "){
-//     //super();
-//     this.x = space.x + random(1, Math.floor(space.w/3))
-//     this.y = space.y + random(1, Math.floor(space.h/3))
-//     this.w = space.w - (this.x - space.x)
-//     this.h = space.h - (this.y - space.y)
-//     this.w -= random(0, this.w/4)
-//     this.h -= random(0, this.h/4)
-//     }
-//   }
-
 
   function numbers(grid){
     var room = {
-      lengthy: Math.random() *grid.length/4 +4,
-      lengthx: Math.random() *grid.length/4 +4
+    lengthy: Math.random() *grid.length/4 +4,
+    lengthx: Math.random() *grid.length/4 +4
     }
     room.y = Math.random() *(grid[1].length - room.lengthy),
     room.x = Math.random() *(grid[1].length - room.lengthx),
@@ -149,11 +76,10 @@ function draw(grid){
     room.lengthx = Math.floor(room.lengthx);
     room.y = Math.floor(room.y);
     room.x = Math.floor(room.x);
-    console.log(room)
     return room;
 }
 
-  function room(numbers, grid){
+  function room(numbers, grid, element , name = " "){
      var room = numbers(grid);
      for (var i = 0; i < grid.length; i++) {
        if(i >= room.y && i <= (room.y + room.lengthy)){
@@ -170,7 +96,7 @@ function draw(grid){
       if(i >= room.y && i <= (room.y + room.lengthy)){
         for (var j = 0; j < grid[0].length; j++) {
             if(j >= room.x && j <= (room.x + room.lengthx)){
-              grid[i][j] = " ";
+                grid[i][j] = new element(name);
             }
           }
         }
@@ -178,15 +104,14 @@ function draw(grid){
       return draw(grid);
     }
 
-
   var grid = grid({x:100,y:200}, Dungeon);
-// this is what physiaclly draws the grid
+  // this is what physiaclly draws the grid
   document.getElementById("blah").innerHTML = draw(grid);
-// this calls the update function
-// this calls the update function
+  // this calls the update function
+  // this calls the update function
   document.getElementById("button").onclick = function(){
-    for (var i = 0; i < 300; i++) {
-  document.getElementById("blah").innerHTML = room(numbers, grid);
+    for (var i = 0; i < 900; i++) {
+  document.getElementById("blah").innerHTML = room(numbers, grid, Dungeon);
   console.log("Clicky");
   // this calls the update function
     }
