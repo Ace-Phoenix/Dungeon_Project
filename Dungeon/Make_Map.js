6e986fa4927b7717d795c38634b375999b5fd192
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-03-12 10:28:08 -0400

    comment fixes not done yet

--- Dungeon/Make_Map.js
+++ Dungeon/Make_Map.js
@@ -12,31 +12,31 @@
     // this is array.push
     make.push([]);
   for (var j = 0; j < size.y; j++) {
-    // this is to push the new element into the array so it has the name and
-    // grid locations
+// this is to push the new element into the array so it has the name and
+// grid locations
     make[i].push(new element(name,[i],[j]));
     }
   }
-    // this is going to be the grid[i][j
-    return make;
-  }
+ // this is going to be the grid[i][j]
+ return make;
+}
 
-    // this is going to be the fucntion thats draws the grid
-    function draw(grid){
-    var design = "&#8943";
-    var background = "&#8942";
+// This is the Function that draws grid and its boarder
+function draw(grid){
+  var design = "&#8943";
+  var background = "&#8942";
   // this is going to be the text variable
-    var text = "";
+  var text = "";
   // this the variable for the grid length so we can store it
-    var lengt = grid.length;
+  var lengt = grid.length;
   // this is going to be the legth of the gird times 2
   var top = lengt;
-  var fixtop = grid[0].length/1.6;
+  var fixtop = grid[0].length/1.58;
   // this is so text knows what to use for the grid
-    text += design;
-  // this makes the top border
-  for (var i = 0; i < fixtop; i++) {
-    top = text += design;
+  text += design;
+    // this makes the top border
+    for (var i = 0; i < fixtop; i++) {
+  top = text += design;
 
   }
   // this is so the grid is square for now
@@ -46,30 +46,24 @@
   // this for satement is for the sides of the grid usaing pipes
   for (var i = 0; i < lengt; i++) {
     text += background;
-
   for (var j = 0; j < grid[0].length; j++) {
     text += "" + grid[i][j] ;
-
   }
-  text += background + "<BR>";
-
+    text += background + "<BR>";
   }
-  text += design ;
-
+    text += design ;
   for (var i = 0; i < fixtop; i++) {
-  top = text += design;
-
+    top = text += design;
   }
   text += design;
-// return the text strings so its appears on the screen
-return text;
+ // return the text strings so its appears on the screen
+ return text;
 }
-//Numbers makes the random numbers for the rooms to be
-//able to draw it in random lengths and random places
-
+  //Numbers makes the random numbers for the rooms to be
+  //able to draw it in random lengths and random places
   function numbers(grid){
+    //the random lengths of the rooms
     var room = {
-      //the random lengths of the rooms
       //Length y
       lengthy: Math.random() *grid.length/15 +1,
       //Length x
@@ -92,41 +86,43 @@ return text;
     return room;
 }
 
+  //This Funstion draws the rooms randomly
+  //using Function nunmbers, grid, and the thing
+  //in grid as well as element Dungeon
   function room(numbers, grid, element , name = " "){
-  // the function that draws the rooms randomly
     //this gives room from function numbers to function room
-     var room = numbers(grid);
-     //this is what makes it so that they do not overlap
-     for (var i = 0; i < grid.length; i++) {
-       // this outer for statement is to check the grid.x for room locations
-       if(i >= room.y && i <= (room.y + room.lengthy)){
-         // this if statement is if there is any rom locations
-         for (var j = 0; j < grid[0].length; j++) {
-           // this it to check the grid.y for anything
-             if(j >= room.x && j <= (room.x + room.lengthx)){
-               //this if satement is what check room locations on the grid
-                 if(grid[i][j] == " "){
-                   // this is to check overlap
-                    return draw(grid);
-                   }
-                 }
-               }
-             }
-           }
-    //this draws the room after finding a location that makes it so they do not overlap
+    var room = numbers(grid);
+    //this is what makes it so that they do not overlap
     for (var i = 0; i < grid.length; i++) {
-      // this for statement is what chscks the grid length
+      // this outer for statement is to check the grid.x for room locations
       if(i >= room.y && i <= (room.y + room.lengthy)){
-        //this if statement checks foe the room .length to generate the room
+      // this if statement is if there is any rom locations
         for (var j = 0; j < grid[0].length; j++) {
-            if(j >= room.x && j <= (room.x + room.lengthx)){
-              // this check the length of the room and spwans the rooms
-                grid[i][j] = new element(name);
+          // this it to check the grid.y for anything
+          if(j >= room.x && j <= (room.x + room.lengthx)){
+          //this if satement is what check room locations on the grid
+          if(grid[i][j] == " "){
+       // this is to check overlap
+       return draw(grid);
+       }
+      }
+      }
+     }
+    }
+    //this draws the room after finding a location that makes it so they do not overlap
+    //these for statement is what checks the grid length
+    //these if statement checks for the room.length to generate the room
+    for (var i = 0; i < grid.length; i++) {
+      if(i >= room.y && i <= (room.y + room.lengthy)){
+        for (var j = 0; j < grid[0].length; j++) {
+          if(j >= room.x && j <= (room.x + room.lengthx)){
+            //This uses the lengths off the room and make it part of the grid
+            grid[i][j] = new element(name);
             }
           }
         }
       }
-      // this returns the updated grid... yay 
+      // this returns the updated grid
       return draw(grid);
     }
 
@@ -134,21 +130,15 @@ return text;
   // this is what physiaclly draws the grid
   document.getElementById("blah").innerHTML = draw(grid);
   // this calls the update function
-  // this calls the update function
   document.getElementById("Rooms").onclick = function(){
-<<<<<<< HEAD
     for (var i = 0; i < 50; i++) {
-=======
-    for (var i = 0; i < 900; i++) {
-  document.getElementById("blah").innerHTML = draw(grid);
->>>>>>> 5adabf6ad12b6c4523d9dd705634fff59d657803
   document.getElementById("blah").innerHTML = room(numbers, grid, Dungeon);
   console.log("Rooms Made");
   // this calls the update function
     }
   }
   document.getElementById("Reset").onclick = function(){
-  document.getElementById("blah").innerHTML = draw(grid)
+  document.getElementById("blah").innerHTML = draw(grid);
   console.log("Reset");
   console.log("still need to work on it");
   // this calls the update function
